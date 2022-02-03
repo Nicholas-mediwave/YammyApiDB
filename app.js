71f7a1d0b3a3b74c23fe17e59db534a47e8a8aad
Nicholas-mediwave <nicholas@mindwaveventures.com>
2022-02-03 11:59:57 +0530

    Added the Express and it listen on Port 1997

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 1997;
+const url = `http://localhost:` + port;
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  console.log(`Server is runing on ${url}`);
+});
+
 console.log("------------------------------------------------------------");
